b93dbbbf1f285b505782e8c7b4545c687053be3f
paul449 <paul_bilbatua@hotmail.com>
2024-04-25 10:27:37 -0500

    connection

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// connect with sequelize
+const Sequelize = require('sequelize');
+const dotenv = require('dotenv').config();
+
+let sequelize;
+
+if(process.env.DB_URL){
+    sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host:'localhost',
+        dialect: 'postgres'
+    }
+  )
+}
