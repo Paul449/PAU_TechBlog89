d9cb485323faab67d4a268a01e368c39b3edf069
paul449 <paul_bilbatua@hotmail.com>
2024-05-10 17:41:48 -0500

    connect DB

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 //importing sequelize library
 const {Sequelize} = require('sequelize');
+//importing and connecting with dotenv library
+require('dotenv').config();
 //connecting to database using sequelize
 const sequelize = process.env.DB_URL
 ? new Sequelize(process.env.DB_URL)
@@ -9,17 +11,17 @@ const sequelize = process.env.DB_URL
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host:'localhost',
+        host:process.env.DB_HOST,
         dialect:'postgres',
+        port:process.env.DB_PORT,
     }
 );
-
 //testing connection with database, using authenticate
 try{
-    sequelize.authenticate(); // test connection
+    await sequelize.authenticate(); // test connection
     console.log("Your connection to database is stable");
 } catch(error){ //throw error if unable to connect
     console.error('Your connection is unstable:', error);
 }
-
+//exporting sequelize database connection to use with models
 module.exports = sequelize;
