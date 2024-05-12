7f62398117631037e60235a9dadcb0820d211381
paul449 <paul_bilbatua@hotmail.com>
2024-05-12 11:36:08 -0500

    server2

--- server.js
+++ server.js
@@ -5,5 +5,17 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 //import sequelize connection
 const sequelize = require('./config/connection');
+//port number
+const PORT = 3002;
 //calling express in our app
 const app = express();
+//handlebars reference to app
+app.engine('main',require('ejs').renderFile)
+//render handlebar template
+app.get('/',async(req,res)=>{
+    res.render('main.handlebars')
+});
+//listening through port 3002
+app.listen(PORT,async()=>{
+    console.log(`listening on port ${PORT}`)
+});
